8286f54ce0cbf3833d5f9c30714c6849ed5c9c69
Kyle Furey <kylepfurey@gmail.com>
2026-03-24 15:56:48 -0400

    Initial Commit

---
+++ include/External.h
@@ -0,0 +1,24 @@
+// .h
+// TODO External Library Includes
+// by NAME
+
+#ifndef TODO_EXTERNAL_H
+#define TODO_EXTERNAL_H
+
+// C STANDARD LIBRARY
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+#include <assert.h>
+
+// EXTERNAL LIBRARIES
+#include <SDL.h>
+
+#endif // TODO_EXTERNAL_H
---
+++ include/Test.h
@@ -0,0 +1,21 @@
+// .h
+// TODO Test Function
+// by NAME
+
+#ifndef TODO_TEST_H
+#define TODO_TEST_H
+
+#include <Export.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Test export function. */
+TODO_API void TODO_test(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TODO_TEST_H
